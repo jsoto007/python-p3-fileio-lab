138ffb06e91a3e894e7c4f74ba7ecac60d3395cb
Jose Soto <jsoto7087@gmail.com>
2024-11-22 11:00:43 -0500

    fist test passing"

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,5 +1,10 @@
+
+
 def write_file(file_name, file_content):
-    pass
+
+    with open(f"{file_name}.txt", 'w', encoding='utf-8') as file:  
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
     pass
