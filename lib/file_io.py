115d21a2552d5d2bbb7fb2cfed15b93d14a470e4
Jose Soto <jsoto7087@gmail.com>
2024-11-22 11:08:37 -0500

    completed test

--- lib/file_io.py
+++ lib/file_io.py
@@ -7,7 +7,11 @@ def write_file(file_name, file_content):
 
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a', encoding='utf-8') as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', encoding='utf-8') as file: 
+       return file.read()
+        
